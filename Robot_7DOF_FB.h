7effe073315612b0b2b457847048604e539298c4
stanley1228 <stanleyfreeze@gmail.com>
2017-06-30 21:40:31 +0800

    加入synwrite romsetting

--- Robot_7DOF_FB.h
+++ Robot_7DOF_FB.h
@@ -11,16 +11,6 @@
 //==========
 #define MAX_AXIS_NUM  7
 
-enum{
-	AXIS1=0,
-	AXIS2,
-	AXIS3,
-	AXIS4,
-	AXIS5,
-	AXIS6,
-	AXIS7
-};
-
 //===========================
 //==Axis index,ID,NO mapping
 //==========================
@@ -81,10 +71,11 @@ static const unsigned char gMapAxisID[MAX_AXIS_NUM]=
 //==Unit transform
 //=================
 #define DEF_PI (3.1415926F)
-#define DEF_RATIO_PUS_TO_DEG (0.0879F)			//360/4096
-#define DEF_RATIO_PUS_TO_RAD (0.0015F)		 //2pi/4096   0.00153398078788564122971808758949
+#define DEF_RATIO_PUS_TO_DEG (0.0879F)		//360/4096
+#define DEF_RATIO_PUS_TO_RAD (0.0015F)		//2pi/4096   0.00153398078788564122971808758949
 #define DEF_RATIO_DEG_TO_PUS (11.3778F)		//4096/360
 #define DEF_RATIO_DEG_TO_RAD (DEF_PI/180)		//pi/180	0.01745329251994329576923690768489 (0.0175F)
+#define DEF_RATIO_RAD_TO_DEG (180/DEF_PI)	
 #define DEF_RATIO_RAD_TO_PUS (651.8986F)	//4096/2pi	651.89864690440329530934789477382
 
 
@@ -112,27 +103,27 @@ enum{
 //==robot angle limit==//
 #define AXIS1_ROBOT_LIM_DEG_L (-80)
 #define AXIS1_ROBOT_LIM_DEG_H 180
-#define AXIS2_ROBOT_LIM_DEG_L (-18)
-#define AXIS2_ROBOT_LIM_DEG_H 180
-#define AXIS3_ROBOT_LIM_DEG_L (-225)
-#define AXIS3_ROBOT_LIM_DEG_H 105
+#define AXIS2_ROBOT_LIM_DEG_L (-180)
+#define AXIS2_ROBOT_LIM_DEG_H 10
+#define AXIS3_ROBOT_LIM_DEG_L (-105)
+#define AXIS3_ROBOT_LIM_DEG_H 250
 #define AXIS4_ROBOT_LIM_DEG_L 0	//need to test
-#define AXIS4_ROBOT_LIM_DEG_H 114	//need to test
-#define AXIS5_ROBOT_LIM_DEG_L (-180)
-#define AXIS5_ROBOT_LIM_DEG_H 148
-#define AXIS6_ROBOT_LIM_DEG_L (-60)
+#define AXIS4_ROBOT_LIM_DEG_H 180	//need to test
+#define AXIS5_ROBOT_LIM_DEG_L (-30)
+#define AXIS5_ROBOT_LIM_DEG_H 90
+#define AXIS6_ROBOT_LIM_DEG_L (-37)
 #define AXIS6_ROBOT_LIM_DEG_H 90
-#define AXIS7_ROBOT_LIM_DEG_L (-30)
-#define AXIS7_ROBOT_LIM_DEG_H 30
+#define AXIS7_ROBOT_LIM_DEG_L (-180)
+#define AXIS7_ROBOT_LIM_DEG_H 180
 
 
-//==robot TORQUE limit==//
+//==robot TORQUE limit==//  0~1023
 #define AXIS1_MAX_TORQUE 55
 #define AXIS2_MAX_TORQUE 55
 #define AXIS3_MAX_TORQUE 55
 #define AXIS4_MAX_TORQUE 55
 #define AXIS5_MAX_TORQUE 55
-#define AXIS6_MAX_TORQUE 55
+#define AXIS6_MAX_TORQUE 200
 #define AXIS7_MAX_TORQUE 55
 
 static const unsigned short int gr2m_offset_pulse[MAX_AXIS_NUM]=
@@ -168,6 +159,34 @@ static const float grobot_lim_rad_H[MAX_AXIS_NUM]=
 	AXIS6_ROBOT_LIM_DEG_H*DEF_RATIO_DEG_TO_RAD,
 	AXIS7_ROBOT_LIM_DEG_H*DEF_RATIO_DEG_TO_RAD
 };
+
+static const float grobot_lim_pus_L[MAX_AXIS_NUM]=
+{
+	AXIS1_ROBOT_LIM_DEG_L*DEF_RATIO_DEG_TO_PUS,
+	AXIS2_ROBOT_LIM_DEG_L*DEF_RATIO_DEG_TO_PUS,
+	AXIS3_ROBOT_LIM_DEG_L*DEF_RATIO_DEG_TO_PUS,
+	AXIS4_ROBOT_LIM_DEG_L*DEF_RATIO_DEG_TO_PUS,
+	AXIS5_ROBOT_LIM_DEG_L*DEF_RATIO_DEG_TO_PUS,
+	AXIS6_ROBOT_LIM_DEG_L*DEF_RATIO_DEG_TO_PUS,
+	AXIS7_ROBOT_LIM_DEG_L*DEF_RATIO_DEG_TO_PUS
+};
+
+static const float grobot_lim_pus_H[MAX_AXIS_NUM]=
+{
+	AXIS1_ROBOT_LIM_DEG_H*DEF_RATIO_DEG_TO_PUS,
+	AXIS2_ROBOT_LIM_DEG_H*DEF_RATIO_DEG_TO_PUS,
+	AXIS3_ROBOT_LIM_DEG_H*DEF_RATIO_DEG_TO_PUS,
+	AXIS4_ROBOT_LIM_DEG_H*DEF_RATIO_DEG_TO_PUS,
+	AXIS5_ROBOT_LIM_DEG_H*DEF_RATIO_DEG_TO_PUS,
+	AXIS6_ROBOT_LIM_DEG_H*DEF_RATIO_DEG_TO_PUS,
+	AXIS7_ROBOT_LIM_DEG_H*DEF_RATIO_DEG_TO_PUS
+};
+
+//=================================
+//==morot max pulse in joint mode
+//=================================
+#define DEF_JOINT_MODE_MAX_PULSE 4095
+#define DEF_JOINT_MODE_MIN_PULSE 0
 //=================================
 //==morot max pulse in joint mode
 //=================================
@@ -189,9 +208,11 @@ static const float grobot_lim_rad_H[MAX_AXIS_NUM]=
 
 
 
-#define DEF_VERY_SMALL (1.e-5)
+#define DEF_VERY_SMALL (1.e-5)//很小的量判斷為0使用
 
+//==========
 //Function
+//==========
 unsigned char getMapAxisNO(unsigned char index); //index 0~ (MAX_AXIS_NUM-1)
 unsigned char getMapAxisID(unsigned char index);
 int ROM_Setting();
@@ -202,11 +223,14 @@ float norm(const Matrix& v);
 Matrix Rogridues(float theta,const Matrix& V_A);
 int IK_7DOF_nonFB(const float l1,const float l2,const float l3,const float x_base,const float y_base,const float z_base,const float x_end,const float y_end,const float z_end,const float alpha,const float beta,const float gamma,const float Rednt_alpha,float* theta);
 int IK_7DOF_FB7roll(const float linkL[6],const float base[3],const float Pend[3],const float PoseAngle[3],const float Rednt_alpha,float* out_theta);
+bool AngleOverConstrain(const float theta[MAX_AXIS_NUM],int *OverIndex);
 
 
-
-
-
+//dynamixel use
+int syncWrite_x86(unsigned short int start_addr, unsigned short int data_length, unsigned short int *param, unsigned short int param_length); // WORD(16bit) syncwrite() for DXL
+int setPosition_x86(int ServoID, int Position, int Speed);//stanley
+int DXL_Initial_x86();
+int DXL_Terminate_x86();
 
 
 
